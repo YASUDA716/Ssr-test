f030971cecbf128936e6158370e81f5d0b826bb9
YASUDA716 <82496037+YASUDA716@users.noreply.github.com>
2024-08-13 00:57:58 +0900

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>自己紹介</h1>
+        <ul>
+          <li>名前：安田</li>
+          <li>住所：日本</li>
+          <li>趣味：散歩</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
